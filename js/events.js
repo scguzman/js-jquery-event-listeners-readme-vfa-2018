2706090d36c07e1ab0e4cb5704c02176a8e761fa
Stephanie Guzman <guzman.stephaniec@gmail.com>
2018-07-02 18:46:58 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,27 +10,22 @@ $('p').on;"keypress", function(getIt){
   };
 
 /*The function should bind the load event that adds the class tasty to the image to add a red frame to the image.*/
-function frameIt() {
   $('img').on('load', function(frameIt){
   img.tasty
 });
-}
 
 /* The function should bind a keydown event to the input field of the form that alerts a user when they have pressed the G key.*/
-function pressIt() {
+
   $(document).on('keypress', function(pressIt) {
   if (pressIt.which == 71){
     alert('\'G\' was pressed');
   }
 });
-}
 
 /*The function should bind a submit event to the form that alerts "Your form is going to be submitted now."*/
-function submitIt() {
+  
   $("form").on("submit", function() {
     alert('your form is going to be submitted now');
-    return;
 });
-}
 
 });
